46cb0133d6757da735ac66e67f8ee202b46ac6e3
gaui74 <32554161+gaui74@users.noreply.github.com>
2018-01-01 22:38:18 +0100

    Verschillende clusterings methoden
    Hieruit is een overwogen keuze gemaakt voor clusterherkenningsalgoritme.

--- Programmeren/Python scripts/ClusterHerkenning.py
+++ Programmeren/Python scripts/ClusterHerkenning.py
@@ -13,7 +13,6 @@ import sklearn.cluster as cluster
 import time
 import hdbscan
 
-#%matplotlib inline
 sns.set_context('poster')
 sns.set_color_codes()
 plot_kwds = {'alpha' : 0.25, 's' : 80, 'linewidths':0}
